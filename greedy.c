2c7adbb0e33c8314788da034602d1025a7465012
ZahinRakin <bsse1447@iit.du.ac.bd>
2023-08-14 01:28:05 +0600

    my version

--- greedy.c
+++ greedy.c
@@ -1,93 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
-    int i;
-    struct node* left;
-    struct node* right;
-};
+typedef struct node{
+  int value;
+  struct node* left;
+  struct node* right;
+}Node;
 
-struct node* createNode(int);
-void buildHuffmanTree(struct node**, int);
-void printHuffmanCodes(struct node*, int[], int);
-void freeHuffmanTree(struct node*);
+Node* head = NULL;
 
-struct node* head = NULL;
+void buildTree(int*, int);
+Node *createNode(int);
+void printTree(Node*, int*, int);
+void swap();
 
-int main() {
-    int arr[] = {3, 34, 2, 5, 23, 54, 36, 10};
-    int size = sizeof(arr) / sizeof(*arr);
-    struct node* arrNode[size];
-
-    for (int i = 0; i < size; i++) {
-        arrNode[i] = createNode(arr[i]);
-    }
-
-    buildHuffmanTree(arrNode, size);
-
-    int codes[100];
-    printHuffmanCodes(head, codes, 0);
-
-    freeHuffmanTree(head);
-
-    return 0;
+int main(){
+  int codes[100];
+  int arr[] = {4,2,9,8,7,5,6};
+  int size = sizeof(arr)/sizeof(*arr);
+  buildTree(arr, size);
+  printf("Huffman codes:\n");
+  printTree(head,codes,0);
+  return 0;
 }
 
-void buildHuffmanTree(struct node** arrNode, int size) {
-    int iterator = 0;
-    while (iterator < size - 1) {
-        for (int i = iterator; i < size - 1; i++) {
-            for (int j = i + 1; j < size; j++) {
-                if (arrNode[i]->i > arrNode[j]->i) {
-                    struct node* temp = arrNode[i];
-                    arrNode[i] = arrNode[j];
-                    arrNode[j] = temp;
-                }
-            }
-        }
-        struct node* newNode = createNode(arrNode[iterator]->i + arrNode[iterator + 1]->i);
-        newNode->left = arrNode[iterator];
-        newNode->right = arrNode[iterator + 1];
-        arrNode[iterator+1] = newNode;
-        iterator++;
-    }
-    head = arrNode[size-1]; // Move this line outside the loop
+void buildTree(int* arr, int size){
+  Node* arrNode[size];
+  for(int i = 0; i < size; i++)
+    arrNode[i] = createNode(arr[i]);
+  for(int iterator = 0; iterator < size - 1; iterator++){
+    for(int i = iterator; i < size - 1; i++)
+      for(int j = i+1; j < size; j++)
+        if(arrNode[i]->value > arrNode[j]->value)
+          swap(arrNode, i, j);
+    Node *newNode = createNode(arrNode[iterator]->value + arrNode[iterator+1]->value);
+    newNode->left = arrNode[iterator];
+    newNode->right = arrNode[iterator + 1];
+    arrNode[iterator + 1] = newNode;
+  }
+  head = arrNode[size-1];
 }
 
-
-void printHuffmanCodes(struct node* root, int codes[], int top) {
-    if (root->left) {
-        codes[top] = 0;
-        printHuffmanCodes(root->left, codes, top + 1);
-    }
-    if (root->right) {
-        codes[top] = 1;
-        printHuffmanCodes(root->right, codes, top + 1);
-    }
-    if (!(root->left) && !(root->right)) {
-        printf("Value: %d, Huffman Code: ", root->i);
-        for (int i = 0; i < top; i++) {
-            printf("%d", codes[i]);
-        }
-        printf("\n");
-    }
+Node *createNode(int value){
+  Node* newNode = malloc(sizeof(Node));
+  newNode->value = value;
+  newNode->left = NULL;
+  newNode->right = NULL;
+  return newNode;
 }
 
-struct node* createNode(int value) {
-    struct node* newNode = (struct node*)malloc(sizeof(struct node));
-    if (newNode != NULL) {
-        newNode->i = value;
-        newNode->left = NULL;
-        newNode->right = NULL;
+void printTree(Node* head, int *codes, int top){
+  if(head->left){
+    codes[top] = 0;
+    printTree(head->left, codes, top+1);
+  }
+  else if(head->right){
+    codes[top] = 1;
+    printTree(head->right, codes, top+1);
+  }
+  else if(!head->left && !head->right){
+    printf("number: %d.\tcode: ",head->value);
+    for(int i = 0; i < top; i++){
+      printf("%d",codes[i]);
     }
-    return newNode;
+    printf("\n");
+  }
 }
 
-void freeHuffmanTree(struct node* root) {
-    if (root) {
-        freeHuffmanTree(root->left);
-        freeHuffmanTree(root->right);
-        free(root);
-    }
+void swap(Node** arrNode, int i, int j){
+  int temp = arrNode[i]->value;
+  arrNode[i]->value = arrNode[j]->value;
+  arrNode[j]->value = temp;
 }
-
